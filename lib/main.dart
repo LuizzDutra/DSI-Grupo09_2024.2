e96390ac71f5a35f7842f6e82cf8138aa4308039
LuizzDutra <luizzidutra@gmail.com>
2024-11-03 21:52:12 -0300

    Migração do código que foi feito em outro ambiente flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'package:iirjdart/butterworth.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +20,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +31,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +38,131 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  AndroidDeviceInfo? _androidInfo;
+
+  UserAccelerometerEvent? _accelerometerEvent;
+  final Duration _accelerometerSampling = const Duration(milliseconds: 10);
+  final _streamSubscription = <StreamSubscription<dynamic>>[];
+
+  double accelEuclidianVal = 0.0;
+
+  List<FlSpot> accelPoints = List<FlSpot>.filled(1000, const FlSpot(0.0, 0.0));
+  int pointsIdx = 0;
+  bool fullSize = false;
+
+  Butterworth butterworth = Butterworth();
+  double lastFilterValue = 0.0;
+    
+
+
+  @override
+  initState(){
+    super.initState();
+    getAndroidModel();
+    initAccelerometer();
+    initFilter();
+  }
+
+  void initFilter(){
+    butterworth.lowPass(4, 1000/_accelerometerSampling.inMilliseconds, 2);
+  }
+
+  Future<void> getAndroidModel() async{
+    AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+          _androidInfo = androidInfo;
     });
   }
 
+  Future<void> initAccelerometer() async{
+    _streamSubscription.add(
+      userAccelerometerEventStream(samplingPeriod: _accelerometerSampling).listen(
+      (UserAccelerometerEvent event) {
+        setState((){
+          _accelerometerEvent = event;
+          accelEuclidianVal = sqrt(pow(_accelerometerEvent!.x, 2) + pow(_accelerometerEvent!.y, 2) + pow(_accelerometerEvent!.z, 2));
+          lastFilterValue = butterworth.filter(accelEuclidianVal);
+          if (pointsIdx < accelPoints.length){
+            accelPoints[pointsIdx] = FlSpot(pointsIdx.toDouble(), lastFilterValue);
+            pointsIdx++;
+          }else{pointsIdx = 0;}
+
+      });
+      },
+      cancelOnError: true,
+      )
+    );
+  }
+
+  
+  Future<void> popUpError(String e) async{
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Error"),
+          content: Text(
+              e),
+        );
+      });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Running on: ${_androidInfo?.model}'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Accelerometer'
+              ),
+            Text(
+              '${_accelerometerEvent?.x.toStringAsFixed(3)} ${_accelerometerEvent?.y.toStringAsFixed(3)} ${_accelerometerEvent?.z.toStringAsFixed(3)}'
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              accelEuclidianVal.toString()
             ),
+            const Text(
+              "Last filter value"
+            ),
+            Text(
+              lastFilterValue.toString()
+            ),
+            AspectRatio(
+              aspectRatio: 2.0,
+              child: LineChart(
+                LineChartData(
+                  minX: 0,
+                  maxX: 1000.0,
+                  lineBarsData: [
+                    LineChartBarData(
+                      spots: accelPoints,
+                      dotData: const FlDotData(
+                        show: false,
+                      ),
+                    ),
+                  ],
+                ),
+                duration: const Duration(milliseconds: 0),
+              ),
+            ), 
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => popUpError("A"),
+        tooltip: 'Debug',
+        child: const Icon(Icons.developer_board_off),
+      ),
     );
   }
 }
